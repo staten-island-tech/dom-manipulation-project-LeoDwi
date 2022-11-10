b3f6bb7ecbb998db4b4b477fcfae8457b2680ea7
LeoDwi <leod13579@gmail.com>
2022-11-10 10:05:38 -0500

    second commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const DOMSelectors = {
+  button: document.getElementById("btn"),
+  text: document.getElementById("text"),
+  box: document.getElementById("background"),
+  points: document.getElementById(".point"),
+};
+console.log(DOMSelectors);
