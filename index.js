9be12877b016e09863cb329bc3887eeb33218e1f
LeoDwi <leod13579@gmail.com>
2022-11-14 09:47:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,5 @@ const DOMSelectors = {
   button: document.getElementById("btn"),
   text: document.getElementById("text"),
   box: document.getElementById("background"),
-  /*   title: document.getElementById("title"),
-  content1: document.getElementById("content1"),
-  content2: document.getElementById("content2"), */
 };
 console.log(DOMSelectors);
