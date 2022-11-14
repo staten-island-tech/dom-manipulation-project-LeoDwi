cf6dc62a2aeb34e63f0ac335597e40662f2907f9
LeoDwi <leod13579@gmail.com>
2022-11-14 10:14:44 -0500

    fourth commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),
-  text: document.getElementById("text"),
+  title: document.getElementsByClassName("title"),
+  content1: document.getElementsByClassName("content1"),
+  content2: document.getElementsByClassName("content2"),
   box: document.getElementById("background"),
 };
 console.log(DOMSelectors);
