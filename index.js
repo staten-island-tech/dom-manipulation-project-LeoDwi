9792cfc00344dcd3cdc1b483f66bc100dddc0b7b
LeoDwi <leod13579@gmail.com>
2022-11-20 23:05:18 -0500

    eighth commit

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const DOMSelectors = {
   title: document.querySelector(`#title`),
   contentOne: document.querySelector(`#content1`),
   contentTwo: document.querySelector(`#content2`),
+  outputTitle: document.getElementById("card title"),
+  outputContent1: document.getElementById("card content1"),
+  outputContent2: document.getElementById("card content2"),
   box: document.getElementById("background"),
   removeButton: document.getElementById("removeBtn"),
 };
-console.log(DOMSelectors);
 
-DOMSelectors.form.addEventListener("submit", function object(event) {
+DOMSelectors.form.addEventListener("submit", function Card(event) {
+  insertHTML();
+  clearInput();
+  let remove = document.getElementById("removeBtn");
+  event.preventDefault();
+});
+
+function insertHTML() {
   const Card = {
     title: DOMSelectors.title.value,
     contentOne: DOMSelectors.contentOne.value,
@@ -18,14 +27,24 @@ DOMSelectors.form.addEventListener("submit", function object(event) {
   console.log(Card);
   //create object from values
 
-  DOMSelectors.box.insertAdjacentHTML(
+  /*   let cardDiv = document.createElement("div");
+  cardDiv.setAttribute("class", "display-card");
+  DOMSelectors.insertAdjacentElement("afterend", cardDiv); */
+
+  DOMSelectors.outputContent1.insertAdjacentHTML(
     "afterend",
     //make card less poopy
-    `<h2>${Card.title}</h2>`
+    `
+    <div class="display-card">
+    <img class="display-img" src="https://www.gannett-cdn.com/presto/2019/08/08/USAT/dc1fe9c6-d585-42c4-9c69-e31cc46e4c94-ABBEY_ROAD.JPG?auto=webp&crop=1481,834,x0,y265&format=pjpg&width=1200"/>
+    <h2 class="display-artist">${Card.title}</h2>
+    <h3 class="display-album">${Card.contentOne}</h3>
+    <h3 class="display-album">${Card.contentTwo}</h3>
+     <button class="remove btn">Remove</button>
+</div>
+`
   );
-  clearInput();
-  event.preventDefault();
-});
+}
 
 function clearInput() {
   DOMSelectors.title.value = "";
@@ -33,6 +52,10 @@ function clearInput() {
   DOMSelectors.contentTwo.value = "";
 }
 
-DOMSelectors.removeButton.addEventListener("click", function removeCard(event) {
+/* DOMSelectors.removeButton.addEventListener("click", function removeCard(event) {
+  outputTitle.remove();
+  outputContent1.remove();
+  outputContent2.remove();
   event.preventDefault();
 });
+ */
